46d7c9b527c324eb4ce99bb0e5058e40dbae30e2
Moosa <moosa7021@gmail.com>
2023-01-29 17:48:34 +1100

    ground and car loading

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import './App.css';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Ground } from './Ground';
+import { Car } from './Car';
+
+function CarShow() {
+  return(
+    <>
+      <OrbitControls target0={[0, 0.35, 0]} maxPolarAngle={1.45} />
+
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      {/* let color = new Color(0, 0, 0); */}
+      <color args={[0, 0, 0]} attach="background" />
+
+      <Car />
+
+      {/*
+        let spotlight = new Spotlight()
+        spotlight.intensity = 1.5;
+        spotlight.position.set(...)
+      */}
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.001}
+      />
+
+      <spotLight 
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.001}
+      />
+
+      <Ground />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
+  );
+}
+
+export default App;
